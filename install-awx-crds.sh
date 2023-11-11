a48a27ec8ddba38c95be3fa6eb9cc090419b4902
tutugodfrey <godfrey_tutu@yahoo.com>
2023-11-11 23:59:43 +0100

    update script to install awx

--- install-awx-crds.sh
+++ install-awx-crds.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
-VERSION_TAG="2.7.2"
-curl -lO https://raw.githubusercontent.com/ansible/awx-operator/${VERSION_TAG}/deploy/awx-operator.yaml
-kubectl apply -f awx-operator.yaml
+helm repo add awx-operator https://ansible.github.io/awx-operator/
+
+helm repo update
+
+helm search repo awx-operator
+
+helm pull awx-operator/awx-operator
+
+helm template awx-operator awx-operator/awx-operator --version 2.7.1 -f values.yaml --no-hooks --include-crds --output-dir awx/base/
+
+cd awx
+
+kubectl apply -f base/awx-operator/crds   # If not already applied
+
+mv base/awx-operator/templates/* base/
+
+kustomize build overlays
+
+kubectl apply -k overlays
